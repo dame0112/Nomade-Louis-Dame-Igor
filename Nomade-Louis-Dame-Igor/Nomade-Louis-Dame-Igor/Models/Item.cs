b31f136f7e0eb3b4f0304e87d8f091b1e4a32caa
dame0112 <58723018+dame0112@users.noreply.github.com>
2020-11-02 14:51:05 +0100

    items (villes et pays)

--- Nomade-Louis-Dame-Igor/Nomade-Louis-Dame-Igor/Models/Item.cs
+++ Nomade-Louis-Dame-Igor/Nomade-Louis-Dame-Igor/Models/Item.cs
@@ -7,5 +7,6 @@ namespace Nomade_Louis_Dame_Igor.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+      
     }
 }
